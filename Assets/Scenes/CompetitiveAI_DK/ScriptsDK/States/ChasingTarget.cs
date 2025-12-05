b5ba4d6b244e69a2344242b3349093d6a9b9436c
Shlmp <127421834+Shlmp@users.noreply.github.com>
2025-12-05 08:43:58 -0600

    first commit
    Added first commit. Added the base for the AI, using FSM with 4 states (Following the course, Chasing the target, Shooting, Searching for targets) and 4 conditions (Has detected a target, Does player's score > AI, Is target dead, Is target in sight)

---
+++ Assets/Scenes/CompetitiveAI_DK/ScriptsDK/States/ChasingTarget.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ChasingTarget", menuName = "FSM/States/ChasingTarget")]
+public class ChasingTarget : State
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scenes/CompetitiveAI_DK/ScriptsDK/States/FollowCourse.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "FollowCourse", menuName = "FSM/States/FollowCourse")]
+public class FollowCourse : State
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
