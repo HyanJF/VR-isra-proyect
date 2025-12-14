5f8ae8d56b08d3ab5eb1c58eb8a5450def473796
HyanJF <119090055+HyanJF@users.noreply.github.com>
2025-12-14 08:13:18 -0600

    Fix Targets

--- Assets/TargetsGGC/ScoreManager.cs
+++ Assets/TargetsGGC/ScoreManager.cs
@@ -1,13 +1,16 @@
+using TMPro;
 using UnityEngine;
 
 public class ScoreManager : MonoBehaviour
 {
     [Header("Score")]
     [SerializeField] private int destroyedTargets = 0;
+    public TextMeshProUGUI scoreText;
 
     public void OnTargetDestroyed()
     {
         destroyedTargets++;
+        scoreText.text = destroyedTargets + " / 15 " ;
 
         Debug.Log($"Target Destroyed! Total Destroyed: {destroyedTargets}");
     }
