c3f959cb7b62b5fef03732761e60ef2ef4d2deab
HyanJF <119090055+HyanJF@users.noreply.github.com>
2026-01-24 16:41:38 -0600

    FIX IA GAMEMASTER

---
+++ Assets/ScriptHF/GameMasterIA/AIDirector.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class AIDirector : MonoBehaviour
+{
+    [Header("Scene Objects")]
+    public List<GameObject> enemies;
+    public List<GameObject> weapons;
+    public List<GameObject> ammo;
+
+    [Header("Time Thresholds (seconds)")]
+    public float slowTime = 300f;  
+    public float fastTime = 120f;   
+
+    void Start()
+    {
+        ApplyDirectorLogic();
+    }
+
+    void ApplyDirectorLogic()
+    {
+        float lastTime = RunStats.lastRunTime;
+
+        if (lastTime < 0f)
+        {
+            SetAllActive(enemies, true);
+            SetAllActive(weapons, true);
+            SetAllActive(ammo, true);
+            return;
+        }
+
+        if (lastTime > slowTime)
+        {
+            ToggleByPercentage(enemies, 0.5f);
+            SetAllActive(weapons, true);
+            SetAllActive(ammo, true);
+        }
+        else if (lastTime < fastTime)
+        {
+            SetAllActive(enemies, true);
+            ToggleByPercentage(weapons, 0.5f);
+            ToggleByPercentage(ammo, 0.5f);
+        }
+        else
+        {
+            SetAllActive(enemies, true);
+            SetAllActive(weapons, true);
+            SetAllActive(ammo, true);
+        }
+    }
+
+    void SetAllActive(List<GameObject> list, bool state)
+    {
+        foreach (var obj in list)
+            if (obj != null)
+                obj.SetActive(state);
+    }
+
+    void ToggleByPercentage(List<GameObject> list, float percentage)
+    {
+        int activeCount = Mathf.RoundToInt(list.Count * percentage);
+
+        SetAllActive(list, false);
+
+        for (int i = 0; i < activeCount; i++)
+        {
+            list[i].SetActive(true);
+        }
+    }
+}
---
+++ Assets/ScriptHF/GameMasterIA/RunStats.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public static class RunStats
+{
+    public static float lastRunTime = -1f;
+}
+
